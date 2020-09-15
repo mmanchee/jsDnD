328bc3d525d1846032127fc1f2bb4457df3b5e71
Brian Harvey <brian.harv3y@gmail.com>
2020-09-15 16:15:21 -0700

    end of day 2 commit

--- src/js/player.js
+++ src/js/player.js
@@ -178,12 +178,49 @@ class WizardCharacter {
     this.img = "https://i.pinimg.com/originals/54/5e/cb/545ecb06834f6fe5afd5faebcae19a5a.png";
   }
 }
+/*
+class CharacterStats {
+  constructor(strength, dexterity, intelligence, wisdom, charisma, constitution) {
+    this.strength = strength;
+    this.strBonus = this.bonusCalc(strength);
+    this.dexterity = dexterity;
+    this.dexBonus = this.bonusCalc(dexterity);
+    this.intelligence = wisdom;
+    this.intBonus = this.bonusCalc(intelligence);
+    this.wisdom = wisdom;
+    this.wisBonus = this.bonusCalc(wisdom);
+    this.charisma = charisma;
+    this.chaBonus = this.bonusCalc(charisma);
+    this.constitution = constitution;
+    this.conBonus = this.bonusCalc(constitution);
+  }
 
-export function getCharacter(name, charClass, acBonus, hpBonus, attBonus) {
+    bonusCalc(stat) {
+      let bonus = 0;
+      if (stat > 17) {
+        bonus = 4;
+      } else if (stat > 15) {
+        bonus = 3;
+      } else if (stat > 13) {
+        bonus = 2;
+      } else if (stat > 11) {
+        bonus = 1;
+      } else if (stat > 9 ) {
+        bonus = 0;
+      } else {
+        bonus = -1;
+      }
+  }
+}
+*/
+export function getCharacter(name, charClass, statClass) {
   let character;
+  let acBonus = statClass.dexBonus;
+  let hpBonus = statClass.conBonus;
+  let attBonus = statClass.strBonus;
   switch (charClass) {
   case "barbarian":
-    character = new BarbarianCharacter(name, (10 + acBonus), hpBonus, attBonus);
+    character = new BarbarianCharacter(name, (10 + acBonus), hpBonus, attBonus,);
     break;
   case "bard":
     character = new BardCharacter(name, (10 + acBonus), hpBonus, attBonus);
@@ -228,4 +265,4 @@ export let imageArray = ["https://i.pinimg.com/736x/ca/f3/26/caf3269770ab71cf0ab
 "https://i.pinimg.com/originals/21/2d/71/212d710a1ce457d541cfaa5345534851.png", "https://i.pinimg.com/originals/b1/ce/a3/b1cea385f37b7c271c18da08b0bf574e.png", 
 "https://wizardofthetavern.com/wp-content/uploads/2019/05/warlock.jpg", "https://i.pinimg.com/originals/54/5e/cb/545ecb06834f6fe5afd5faebcae19a5a.png"];
 
-export let descriptionArray
+export let descriptionArray = [];
